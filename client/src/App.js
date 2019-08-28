028f0c9b70f081ec79ee2dbb8229288d8c4e4102
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-28 16:11:34 -0400

    working through bugs on the links and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import ClientLogin from "./components/client/ClientLogin";
 import TrainerLogin from "./components/trainer/TrainerLogin";
 import SignUp from "./components/SignUp/SignUp";
@@ -11,29 +11,19 @@ function App() {
       <header className="App-header">
         <p>Welcome to Anywhere Fitness</p>
       </header>
-      <Switch>
-        <div>
-          <div className="Options">
-            <Route path="/ClientLogin" component={ClientLogin}>
-              <button>Client Log In</button>
-            </Route>
-          </div>
-          <div>
-            <button>
-              <Route path="/TrainerLogin" component={TrainerLogin}>
-                Trainer Log In
-              </Route>
-            </button>
-          </div>
-          <div>
-            <button>
-              <Route path="/SignUp" component={SignUp}>
-                Sign Up
-              </Route>
-            </button>
-          </div>
-        </div>
-      </Switch>
+      <div className="Options">
+        <Link to="/ClientLogin">Client Log In</Link>
+      </div>
+      <div>
+        <Link to="/TrainerLogin">Trainer Log In</Link>
+      </div>
+      <div>
+        <Link path="/SignUp">Sign Up</Link>
+      </div>
+      <Route path="/ClientLogin" component={ClientLogin} />
+      <Route path="/TrainerLogin" component={TrainerLogin} />
+      <PrivateRoute exact path="/protected" component={ClientApp} />
+      <PrivateRoute exact path="/protected" component={TrainerApp} />
     </div>
   );
 }
--- client/src/components/trainer/MyWorkouts.js
+++ client/src/components/trainer/MyWorkouts.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MyWorkouts = props => {
-  return;
+  return (
+    <div className="saved-workouts">
+      <h3>Saved Movies:</h3>
+      {props.workouts.map(workout => (
+        <span className="saved-workout">{workout.title}</span>
+      ))}
+      <div className="home-button">
+        <Link to="/ClientApp" component="ClientApp">
+          Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default MyWorkouts;
